91e78a64490e47bc039c5e1c7a06928a227345ea
Deanna Swallow <swallowdeanna@gmail.com>
2018-01-27 17:11:31 -0700

    Setup Rooms Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAG5Z-iLt6S_tffcQkoz4-abqj2xrM-I-s",
+    authDomain: "blocchatdms.firebaseapp.com",
+    databaseURL: "https://blocchatdms.firebaseio.com",
+    projectId: "blocchatdms",
+    storageBucket: "blocchatdms.appspot.com",
+    messagingSenderId: "1034090268345"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
---
+++ src/components/RoomList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+  }
+
+  render() {
+
+  }
+}
+
+export default RoomList;
