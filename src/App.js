6b0ba7ce7ecace58b5b3e60c88e34bd0639f5c36
Deanna Swallow <swallowdeanna@gmail.com>
2018-01-29 09:16:23 -0700

    Display room names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 
 //Initialize Firebase
 var config = {
@@ -18,13 +18,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <RoomList firebase={firebase}></RoomList>
+        </main>
       </div>
     );
   }
